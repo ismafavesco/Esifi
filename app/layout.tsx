00079e11f8ec5b2a378e0b9c03d0916309408a23
ismael <ismafavesco@gmail.com>
2024-04-01 17:50:22 -0700

    Browser openning

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,38 +1,47 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import { ClerkProvider } from '@clerk/nextjs'
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { ClerkProvider } from '@clerk/nextjs';
+import { ToasterProvider } from '@/components/toaster-provider';
+import { ModalProvider } from '@/components/modal-provider';
+import { CrispProvider } from '@/components/crisp-provider';
+import './globals.css';
 
-import { ToasterProvider } from '@/components/toaster-provider'
-import { ModalProvider } from '@/components/modal-provider'
-import { CrispProvider } from '@/components/crisp-provider'
-
-import './globals.css'
-
-
-
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Esifi',
   description: 'Most Advance AI software',
-}
+};
 
-export default async function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        <CrispProvider />
+        <head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                var ua = navigator.userAgent || navigator.vendor;
+                var isInstagram = (ua.indexOf('Instagram') > -1) ? true : false;
+
+                if (isInstagram) {
+                  if (/iPad|iPhone|iPod/.test(ua)) {
+                    window.location.href = 'x-web-search://?https://esifi-ai.com';
+                  } else {
+                    window.location.href = 'intent:https://esifi-ai.com#Intent;end';
+                  }
+                }
+              `,
+            }}
+          />
+        </head>
         <body className={inter.className}>
+          <CrispProvider />
           <ToasterProvider />
           <ModalProvider />
           {children}
         </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
