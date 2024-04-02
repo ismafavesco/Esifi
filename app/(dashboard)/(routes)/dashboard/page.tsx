341e34ecc0f70412afde54adf4bc0983fde38823
ismael <ismafavesco@gmail.com>
2024-04-01 17:01:08 -0700

    speech to text 3

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -65,26 +65,23 @@ const DashboardPage =() =>{
 
      </div>
      <div className="px-4 md:px-20 lg:px-32 space-y-4">
-        {tools.map((tool) =>(
-            <Card 
-            onClick={()=> router.push(tool.href)}
-            key={tool.href}
-            className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
-            >
-                <div className="flex items-center gap-x-4">
-                    <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
-                        <tool.icon className={cn("w-8 h-8", tool.color)}/>
-
-                    </div>
-                    <div className="font-semibold">
-                        {tool.label}
-
-                    </div>
-
-                </div>
-                <ArrowRight className="w-5 h-5" />
-            </Card>
-        ))}
+     {tools.map((tool) => (
+  tool && (
+    <Card
+      onClick={() => router.push(tool.href)}
+      key={tool.href}
+      className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
+    >
+      <div className="flex items-center gap-x-4">
+        <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+          <tool.icon className={cn("w-8 h-8", tool.color)} />
+        </div>
+        <div className="font-semibold">{tool.label}</div>
+      </div>
+      <ArrowRight className="w-5 h-5" />
+    </Card>
+  )
+))}
 
      </div>
 </div>
