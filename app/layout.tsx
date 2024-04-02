ce2cac1998d959b68771a4a360f7d0bd31666abe
ismael <ismafavesco@gmail.com>
2024-04-01 18:48:32 -0700

    browser 14

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
         <head>
+          <meta name="apple-mobile-web-app-capable" content="yes" />
           <script
             dangerouslySetInnerHTML={{
               __html: `
@@ -25,13 +26,11 @@ export default async function RootLayout({ children }: { children: React.ReactNo
                 var isInstagram = (ua.indexOf('Instagram') > -1) ? true : false;
 
                 if (isInstagram) {
-                  document.body.innerHTML = '<div style="display: flex; justify-content: center; align-items: center; height: 100vh; background-color: #f2f2f2;">' +
-                    '<div style="text-align: center;">' +
-                    '<h1 style="font-size: 24px; margin-bottom: 20px;">Welcome to Esifi</h1>' +
-                    '<p style="font-size: 16px; margin-bottom: 20px;">To access the full Esifi website, please open it in your device\'s web browser.</p>' +
-                    '<a href="https://www.esifi-ai.com" target="_blank" style="display: inline-block; padding: 10px 20px; background-color: #007bff; color: #fff; text-decoration: none; border-radius: 4px; font-weight: bold;">Open in Browser</a>' +
-                    '</div>' +
-                    '</div>';
+                  if (/iPad|iPhone|iPod/.test(ua)) {
+                    window.location.href = 'https://esifi-ai.com';
+                  } else {
+                    window.location.href = 'intent://esifi-ai.com#Intent;end';
+                  }
                 }
               `,
             }}
